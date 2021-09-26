39b316bcb74770dfc261abd3ea9d73205c0f3251
David Ralph <me@davidjcralph.co.uk>
2021-09-26 12:35:07 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,46 +6,46 @@ const ratelimit = require('express-rate-limit');
 // Config
 let config;
 try {
-    config = require('../config.json');
+  config = require('../config.json');
 } catch (error) {
-    throw new Error('Couldn\'t find \'./config.json\'');
+  throw new Error('Couldn\'t find \'./config.json\'');
 }
 
 // Logging
 let log;
 if (config.logging.enabled) {
-    const logger = require('leekslazylogger');
-    log = new logger();
-    app.use(require('leekslazylogger-express'));
+  const logger = require('leekslazylogger');
+  log = new logger();
+  app.use(require('leekslazylogger-express'));
 }
 
 // Ratelimiting
 if (config.ratelimit.enabled) {
-    app.use(ratelimit({
-        windowMs: config.ratelimit.time,
-        max: config.ratelimit.maxRequests
-    }));
+  app.use(ratelimit({
+    windowMs: config.ratelimit.time,
+    max: config.ratelimit.maxRequests
+  }));
 }
 
 // Check if all folders are intact
 if (!fs.existsSync('./routers')) {
-    log.error('Couldn\'t find \'./routers\'');
+  log.error('Couldn\'t find \'./routers\'');
 }
 
 // Hook up all the routers
 fs.readdir('./routers/', (err, files) => {
-    if (err) {
-        log.error(`Failed to load router: ${router}`);
-    }
+  if (err) {
+    log.error(`Failed to load router: ${router}`);
+  }
 
-    files.forEach((file) => {
-        app.use(require(`./routers/${file}`));
-    });
+  files.forEach((file) => {
+    app.use(require(`./routers/${file}`));
+  });
 });
 
 // Hook up the static folder (if enabled)
 if (config.static.enabled) {
-    express.static(config.static.directory);
+  express.static(config.static.directory);
 }
 
 // Listen on port and notify when it is up
--- src/routers/main.js
+++ src/routers/main.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (_req, res) => {
-    res.status(200).send('hello');
+  res.status(200).send('hello');
 });
 
 module.exports = router;
